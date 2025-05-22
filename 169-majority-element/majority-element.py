023dbab74ad42c535001655ab5e72640ce46c8ab
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-05-22 13:02:20 +0530

    Time: 19 ms (17.06%) | Memory: 14.8 MB (6.75%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,14 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        c=0
-        majorityele=None
-        for i in nums:
-            if c==0:
-                majorityele=i
-                c=1
-            elif majorityele==i:
-                c+=1
+        cnt=0
+        el=None
+        for i in range(len(nums)):
+            if cnt==0:
+                cnt=1
+                el=nums[i]
+            elif el==nums[i]:
+                cnt+=1
             else:
-                c-=1
-        return majorityele
+                cnt-=1
+        cnt1=0
+        for i in range(len(nums)):
+            if nums[i]==el:
+                cnt+=1
+        if cnt>(len(nums)/2):
+            return el
+        return -1
