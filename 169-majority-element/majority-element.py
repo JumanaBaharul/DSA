29a6f4d88d1a12ac581d54898ee77611c747c9b9
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-05-22 13:00:17 +0530

    Time: 11 ms (51.62%) | Memory: 14.7 MB (6.75%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return nums[len(nums)//2]
+        cnt=0
+        el=None
+        for i in range(len(nums)):
+            if cnt==0:
+                cnt=1
+                el=nums[i]
+            elif el==nums[i]:
+                cnt+=1
+            else:
+                cnt-=1
+        return el
