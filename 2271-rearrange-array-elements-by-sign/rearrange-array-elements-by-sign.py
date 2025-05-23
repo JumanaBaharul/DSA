8b36f623afc0acd2bb547df71e07a209f1408e6f
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-05-23 13:06:51 +0530

    Time: 71 ms (77.77%) | Memory: 48 MB (16.89%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -4,25 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # n=len(nums)
-        # ans=[0]*n
-        # posindex=0
-        # negindex=1
-        # for i in range(n):
-        #     if nums[i]<0:
-        #         ans[negindex]=nums[i]
-        #         negindex+=2
-        #     else:
-        #         ans[posindex]=nums[i]
-        #         posindex+=2
-        # return ans
-        ansarray=[0]*len(nums)
-        pos,neg=0,1
-        for i in range(len(nums)):
-            if nums[i]>0:
-                ansarray[pos]=nums[i]
-                pos+=2
+        n=len(nums)
+        ans=[0]*n
+        posindex=0
+        negindex=1
+        for i in range(n):
+            if nums[i]<0:
+                ans[negindex]=nums[i]
+                negindex+=2
             else:
-                ansarray[neg]=nums[i]
-                neg+=2
-        return ansarray
+                ans[posindex]=nums[i]
+                posindex+=2
+        return ans
