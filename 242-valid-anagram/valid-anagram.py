700ddebe9ee74b49a20041e7ebcbaed735986bc8
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-06-21 16:11:17 +0530

    Time: 3 ms (98.40%) | Memory: 17.8 MB (63.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,12 +3,7 @@ class Solution:
         if len(s)!=len(t):
             return False
         
-        count_s={}
-        count_t={}
-
-        for ch in s:
-            count_s[ch]=count_s.get(ch,0)+1
-        for ch in t:
-            count_t[ch]=count_t.get(ch,0)+1
-        
-        return count_s==count_t
+        for c in set(s):
+            if s.count(c)!=t.count(c):
+                return False
+        return True
