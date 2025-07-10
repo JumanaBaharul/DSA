ec43a29fc411805ef8bb999fc603c6397f7beb5c
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-07-10 10:08:46 +0530

    Time: 7 ms (83.04%) | Memory: 18 MB (39.53%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans=[]
+        ds=[]
+        def findCombinations(ind:int,target: int):
+            if ind==len(candidates):
+                if target==0:
+                    ans.append(ds[:])
+                return
+            if candidates[ind]<=target:
+                ds.append(candidates[ind])
+                findCombinations(ind,target-candidates[ind])
+                ds.pop()
+            findCombinations(ind+1,target)
+        findCombinations(0,target)
+        return ans
