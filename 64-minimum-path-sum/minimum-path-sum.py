8ff6e8450072328a0eb3e48ad311e0c5c28544bb
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-09-16 21:45:55 +0530

    Time: 15 ms (58.92%) | Memory: 20.2 MB (38.20%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -2,16 +2,21 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        dp=[[-1 for j in range(n)] for i in range(m)]
-        return self.miniPathSum(m-1,n-1,grid,dp)
-    def miniPathSum(self,i:int,j:int,grid: List[List[int]],dp) -> int:   
-        if i==0 and j==0:
-            return grid[0][0]
-        if i<0 or j<0:
-            return float('inf')
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        up=grid[i][j]+self.miniPathSum(i-1,j,grid,dp)
-        left=grid[i][j]+self.miniPathSum(i,j-1,grid,dp)
-        dp[i][j]=min(up,left)
-        return dp[i][j]
+        dp=[[0 for j in range(n)] for i in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    dp[i][j]=grid[i][j]
+                else:
+                    up=grid[i][j]
+                    if i>0:
+                        up+=dp[i-1][j]
+                    else:
+                        up+=float('inf')
+                    left=grid[i][j]
+                    if j>0:
+                        left+=dp[i][j-1]
+                    else:
+                        left+=float('inf')
+                    dp[i][j]=min(up,left)
+        return dp[m-1][n-1]
