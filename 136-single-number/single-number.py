29423a24013e367dd4749263a7a00d48a8712c54
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-10-01 23:12:45 +0530

    Time: 4 ms (45.00%) | Memory: 19.6 MB (42.39%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,6 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        xor=0
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res=0
         for i in range(len(nums)):
-            xor=xor^nums[i]
-        return xor
+            res=res^nums[i]
+        return res
