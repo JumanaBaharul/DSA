0887ae7691e768b5814a883d8ce1e86f408134d9
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2025-10-11 15:26:31 +0530

    Time: 245 ms (89.84%) | Memory: 17.7 MB (96.17%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -3,22 +3,19 @@ class Solution:
         if len(s)==1:
             return s
         start,end=0,0
-
-        def expand_from_center(left:int,right:int)->(int,int):
-            while left>=0 and right<len(s) and s[left]==s[right]:
-                left-=1
-                right+=1
-            return left+1,right-1
         
-        for i in range(len(s)):
-            left1,right1=expand_from_center(i,i)
-            left2,right2=expand_from_center(i,i+1)
-            
-            if right1-left1>end-start:
-                start,end=left1,right1
-            if right2-left2>end-start:
-                start,end=left2,right2
-        
+        for center in range(len(s)):
+            len1=self.expandFromCenter(s,center,center)
+            len2=self.expandFromCenter(s,center,center+1)
+            maxi=max(len1,len2)
+            if maxi>end-start:
+                start=center-(maxi-1)//2
+                end=center+maxi//2
         return s[start:end+1]
 
-        
+    def expandFromCenter(self,s,left,right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return right-left-1
+    
