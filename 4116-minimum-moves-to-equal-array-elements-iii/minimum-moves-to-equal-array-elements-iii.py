c1a3bd13ec0daf733b77c802df56537a84e7ab40
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2026-01-02 17:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (97.68%) - LeetSync

---
+++ 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        max_val=max(nums)
+        moves=0
+
+        for num in nums:
+            moves+=max_val-num
+        return moves
